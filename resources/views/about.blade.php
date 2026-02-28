fcd550a0c075f52ffc69eedbb4fe9226e69877f8
Weellyyy <wildanprtm2304@gmail.com>
2026-02-28 09:35:08 +0700

    install breeze

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <p>Nama: Wildan Zauhair Pratama</p>
+                    <p>NIM: 20230140045</p>
+                    <p>Prodi: Teknik Informatika</p>
+                    <p>Hobi: Main game</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
