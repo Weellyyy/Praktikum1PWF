368186c8b68695107d0c1fa1fa5957d602c003d1
Weellyyy <wildanprtm2304@gmail.com>
2026-03-09 12:11:58 +0700

    Praktikum 3

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name',
+        'qty',
+        'price',
+    ];
+
+    public function kategori()
+    {
+        return $this->hasMany(Kategori::class);
+    }
+}
